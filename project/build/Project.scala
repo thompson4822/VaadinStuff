88ec7b0fdc3b161fe7d3c5c356859a278f484a21
Steve Thompson <thompson4822@gmail.com>
2011-06-07 07:21:10 -0600

    Basic sbt project files for each of the sub-modules.

---
+++ project/build/Project.scala
@@ -0,0 +1,25 @@
+
+import sbt._
+
+class Project(info: ProjectInfo) extends ParentProject(info) {
+/*
+  val vaadinVersion = "6.6.0"
+
+  val jettyDep = "org.mortbay.jetty" % "jetty" % "6.1.18" % "test->default"
+  val vaadinDep = "com.vaadin" % "vaadin" % vaadinVersion
+*/
+
+  lazy val scalaVaadin = project("ScalaVaadin", "ScalaVaadin", new ScalaVaadinProject(_))
+  lazy val spike = project("spike", "spike", new DefaultWebProject(_), scalaVaadin)
+  lazy val bookProject = project("BookProject", "BookProject", new DefaultWebProject(_), scalaVaadin)
+  lazy val addressBookProject = project("AddressBook", "AddressBook", new DefaultWebProject(_), scalaVaadin)
+  
+  class ScalaVaadinProject(info: ProjectInfo) extends DefaultProject(info)
+  {
+    val vaadinVersion = "6.6.0"
+
+    val jettyDep = "org.mortbay.jetty" % "jetty" % "6.1.18" % "test->default"
+    val vaadinDep = "com.vaadin" % "vaadin" % vaadinVersion
+  }
+
+}
