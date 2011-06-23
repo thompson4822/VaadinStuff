5a599d5f276f22e7c1616b42d2340ad7ebda43ac
Steve Thompson <thompson4822@gmail.com>
2011-06-23 05:48:16 -0600

    General updates to the ScalaVaadin library.

--- ScalaVaadin/src/main/scala/org/scalaVaadin/source.scala
+++ ScalaVaadin/src/main/scala/org/scalaVaadin/source.scala
@@ -1,12 +1,14 @@
 package org
 
-import com.vaadin.ui.{Label, Window}
 import scala.xml.Elem
-import java.lang.Boolean
 import com.vaadin.event.ItemClickEvent.ItemClickListener
 import scala.collection.JavaConverters._
+import scala.reflect.Manifest
+import javax.sql.rowset.Predicate
+import com.vaadin.data.Property.{ValueChangeEvent, ValueChangeListener}
 
 package object scalaVaadin {
+  //type AbstractInMemoryContainer[ITEMIDTYPE, PROPERTYIDCLASS, ITEMCLASS] = com.vaadin.data.util.AbstractInMemoryContainer[ITEMIDTYPE, PROPERTYIDCLASS, ITEMCLASS]
   type Alignment = com.vaadin.ui.Alignment
   type BeanItem[A] = com.vaadin.data.util.BeanItem[A]
   type BeanItemContainer[A] = com.vaadin.data.util.BeanItemContainer[A]
@@ -15,6 +17,7 @@ package object scalaVaadin {
   type Component = com.vaadin.ui.Component
   type CustomComponent = com.vaadin.ui.CustomComponent
   type Form = com.vaadin.ui.Form
+  type HierarchicalContainer = com.vaadin.data.util.HierarchicalContainer
   type HorizontalLayout = com.vaadin.ui.HorizontalLayout
   type HorizontalSplitPanel = com.vaadin.ui.HorizontalSplitPanel
   type IntegerValidator = com.vaadin.data.validator.IntegerValidator
@@ -24,9 +27,15 @@ package object scalaVaadin {
   type MenuBar = com.vaadin.ui.MenuBar
   type MenuItem = com.vaadin.ui.MenuBar#MenuItem
   type Panel = com.vaadin.ui.Panel
+  type Property = com.vaadin.data.Property
   type Resource = com.vaadin.terminal.Resource
   type TextArea = com.vaadin.ui.TextArea
   type TextField = com.vaadin.ui.TextField
+  type ThemeResource = com.vaadin.terminal.ThemeResource
+  type TreeCollapseEvent = com.vaadin.ui.Tree#CollapseEvent
+  type TreeCollapseListener = com.vaadin.ui.Tree.CollapseListener
+  type TreeExpandEvent = com.vaadin.ui.Tree#ExpandEvent
+  type TreeExpandListener = com.vaadin.ui.Tree.ExpandListener
   type VaadinApplication = com.vaadin.Application
   type ValueChangeEvent = com.vaadin.data.Property.ValueChangeEvent
   type VerticalLayout = com.vaadin.ui.VerticalLayout
@@ -34,7 +43,13 @@ package object scalaVaadin {
   type Window = com.vaadin.ui.Window
   type WindowCloseEvent = Window#CloseEvent
 
-  // setComponentAlignment(component left center)
+  /*
+  abstractInMemoryContainerToRichAbstractInMemoryContainer(container: com.vaadin.data.util.AbstractInMemoryContainer) = new RichInMemoryAbstractContainer(container)
+
+  class RichInMemoryAbstractContainer(container: com.vaadin.data.util.AbstractInMemoryContainer) {
+    def addContainerProperty() { container.addContainerProperty(<NEED TO PUT STUFF HERE>)}
+  }
+  */
 
   object Alignment {
     val ALIGNMENT_LEFT: Int = 1
@@ -43,6 +58,76 @@ package object scalaVaadin {
     val ALIGNMENT_BOTTOM: Int = 8
     val ALIGNMENT_HORIZONTAL_CENTER: Int = 16
     val ALIGNMENT_VERTICAL_CENTER: Int = 32
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  // RichAbstractComponent
+  //
+  ///////////////////////////////////////////////////////////////////
+  implicit def abstractComponentToRichAbstractComponent(component: com.vaadin.ui.AbstractComponent) =
+    new RichAbstractComponent(component)
+
+  class RichAbstractComponent(component: com.vaadin.ui.AbstractComponent) {
+    def setCaption(caption: Elem) { component.setCaption(caption.toString()) }
+    def setDescription(description: Elem) { component.setDescription(description.toString()) }
+
+    // Would like to add the following:
+    def isVisible(predicate: => Boolean) { }
+    def isEnabled(predicate: => Boolean) { }
+    def isReadOnly(predicate: => Boolean) { }
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  // RichAbstractField
+  //
+  ///////////////////////////////////////////////////////////////////
+  implicit def abstractFieldToRichAbstractField(field: com.vaadin.ui.AbstractField) = new RichAbstractField(field)
+
+  class RichAbstractField(field: com.vaadin.ui.AbstractField) {
+    def addValueChangeEvent(action: ValueChangeEvent => Unit) = field.addListener(new ValueChangeListener(action))
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  // RichAbstractOrderedLayout
+  //
+  ///////////////////////////////////////////////////////////////////
+  implicit def abstractOrderedLayoutToRichAbstractOrderedLayout(layout: com.vaadin.ui.AbstractOrderedLayout) =
+    new RichAbstractOrderedLayout(layout)
+
+  class RichAbstractOrderedLayout(layout: com.vaadin.ui.AbstractOrderedLayout) {
+    class AlignmentContext(component: com.vaadin.ui.Component) {
+      import com.vaadin.ui.Alignment._
+      def topLeft() { layout.setComponentAlignment(component, TOP_LEFT) }
+      def topCenter() { layout.setComponentAlignment(component, TOP_CENTER) }
+      def topRight() { layout.setComponentAlignment(component, TOP_RIGHT) }
+
+      def middleLeft() { layout.setComponentAlignment(component, MIDDLE_LEFT) }
+      def middleCenter() { layout.setComponentAlignment(component, MIDDLE_CENTER) }
+      def middleRight() { layout.setComponentAlignment(component, MIDDLE_CENTER) }
+
+      def bottomLeft() { layout.setComponentAlignment(component, BOTTOM_LEFT) }
+      def bottomCenter() { layout.setComponentAlignment(component, BOTTOM_CENTER) }
+      def bottomRight() { layout.setComponentAlignment(component, BOTTOM_RIGHT) }
+    }
+    def align(component: com.vaadin.ui.Component) = new AlignmentContext(component)
+    def components: Iterator[Component] = layout.getComponentIterator.asScala
+  }
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  // RichAbstractSplitPanel
+  //
+  ///////////////////////////////////////////////////////////////////
+  implicit def abstractSplitPanelToRichAbstractSplitPanel(panel: com.vaadin.ui.AbstractSplitPanel) =
+    new RichAbstractSplitPanel(panel)
+
+  class RichAbstractSplitPanel(panel: com.vaadin.ui.AbstractSplitPanel) {
+    def setSplitPosition(extent: UnitExtent, reverse: Boolean = false) {
+      panel.setSplitPosition(extent.value.toInt, extent.unit, reverse)
+    }
   }
 
   implicit def intToUnitExtent(value: Int) = floatToUnitExtent(value)
@@ -60,7 +145,7 @@ package object scalaVaadin {
   }
 
   case class UnitExtent(value: Float, unit: Int) {
-    override def toString() = {
+    override def toString = {
       value.toString + (unit match {
         case com.vaadin.terminal.Sizeable.UNITS_CM => "cm"
         case com.vaadin.terminal.Sizeable.UNITS_EM => "em"
@@ -75,35 +160,22 @@ package object scalaVaadin {
     }
   }
 
-  class RichSizable(s: com.vaadin.terminal.Sizeable) {
-    def setWidth(extent: UnitExtent) = s.setWidth(extent.value, extent.unit)
-    def setHeight(extent: UnitExtent) = s.setHeight(extent.value, extent.unit)
-  }
+  implicit def sizableToRichSizable(sizable: com.vaadin.terminal.Sizeable) = new RichSizable(sizable)
 
-  implicit def abstractOrderedLayoutToRichAbstractOrderedLayout(layout: com.vaadin.ui.AbstractOrderedLayout) =
-    new RichAbstractOrderedLayout(layout)
+  class RichSizable(component: com.vaadin.terminal.Sizeable) {
+    def getVerticalExtent: UnitExtent = UnitExtent(component.getHeight, component.getHeightUnits)
+    def setVerticalExtent(extent: UnitExtent) { component.setHeight(extent.value, extent.unit) }
 
-  class RichAbstractOrderedLayout(layout: com.vaadin.ui.AbstractOrderedLayout) {
-    class AlignmentContext(component: com.vaadin.ui.Component) {
-      import com.vaadin.ui.Alignment._
-      def topLeft = layout.setComponentAlignment(component, TOP_LEFT)
-      def topCenter = layout.setComponentAlignment(component, TOP_CENTER)
-      def topRight = layout.setComponentAlignment(component, TOP_RIGHT)
+    def getHorizontalExtent: UnitExtent = UnitExtent(component.getWidth, component.getWidthUnits)
+    def setHorizontalExtent(extent: UnitExtent) { component.setWidth(extent.value, extent.unit) }
 
-      def middleLeft = layout.setComponentAlignment(component, MIDDLE_LEFT)
-      def middleCenter = layout.setComponentAlignment(component, MIDDLE_CENTER)
-      def middleRight = layout.setComponentAlignment(component, MIDDLE_CENTER)
+    def width: UnitExtent = UnitExtent(component.getWidth, component.getWidthUnits)
+    def width_=(extent: UnitExtent) { component.setWidth(extent.value, extent.unit) }
 
-      def bottomLeft = layout.setComponentAlignment(component, BOTTOM_LEFT)
-      def bottomCenter = layout.setComponentAlignment(component, BOTTOM_CENTER)
-      def bottomRight = layout.setComponentAlignment(component, BOTTOM_RIGHT)
-    }
-    def align(component: com.vaadin.ui.Component) = new AlignmentContext(component)
-    def components: Iterator[Component] = layout.getComponentIterator.asScala
   }
 
   class MenuBarCommand(action: MenuItem => Unit) extends com.vaadin.ui.MenuBar.Command {
-    def menuSelected(selectedItem: MenuItem) = action(selectedItem)
+    def menuSelected(selectedItem: MenuItem) { action(selectedItem) }
   }
 
   implicit def menuThingToRichMenuThing(menuThing: { def addItem(caption: String, icon: Resource, command: com.vaadin.ui.MenuBar.Command): MenuItem }) = new {
@@ -115,41 +187,80 @@ package object scalaVaadin {
     }
   }
 
-  implicit def abstractSplitPanelToRichAbstractSplitPanel(panel: com.vaadin.ui.AbstractSplitPanel) =
-    new RichAbstractSplitPanel(panel)
 
-  class RichAbstractSplitPanel(panel: com.vaadin.ui.AbstractSplitPanel) {
-    def setSplitPosition(extent: UnitExtent, reverse: Boolean = false) {
-      panel.setSplitPosition(extent.value.toInt, extent.unit, reverse)
+/*
+  object FProperty {
+    def apply[T : Manifest](getter: => T) = new FProperty(getter)
+    def apply[T : Manifest](getter: => T, setter: T => Unit) = new FProperty(getter, setter)
+  }
+
+  class FProperty[T : Manifest] private(getter: => T, setter: T => Unit = null) extends Property {
+    def isReadOnly = setter == null
+    def getType = {
+      val result = classManifest[T].erasure
+      println("Found the following type: " + result)
+      result
     }
-  }
+    def setValue(newValue: AnyRef) {
+      if(setter != null) setter(newValue.asInstanceOf[T])
+      println("Just set the value for this property!")
+    }
+    def getValue = {
+      val value = getter
+      value.asInstanceOf[AnyRef]
+    }
 
-  implicit def abstractComponentToRichAbstractComponent(component: com.vaadin.ui.AbstractComponent) =
-    new RichAbstractComponent(component)
+    def setReadOnly(newStatus: Boolean) {}
 
-  class RichAbstractComponent(component: com.vaadin.ui.AbstractComponent) {
-    def setWidth(extent: UnitExtent) = component.setWidth(extent.toString())
+    override def toString = getValue.toString
   }
+*/
 
   class ValueChangeListener(f: ValueChangeEvent => Unit) extends com.vaadin.data.Property.ValueChangeListener {
     override def valueChange(event:ValueChangeEvent) { f(event) }
   }
 
-  class HtmlLabel(text: String) extends Label(text, Label.CONTENT_XHTML) {
-    def this(html:Elem) = { this(html.toString) }
+  class HtmlLabel(text: String) extends Label(text, com.vaadin.ui.Label.CONTENT_XHTML) {
+    def this(html:Elem) = { this(html.toString()) }
   }
 
   case class ColumnDefinition(field: String, title: String)
 
   class Table extends com.vaadin.ui.Table {
     def addListener(f: ValueChangeEvent => Unit) { addListener(new ValueChangeListener(f)) }
-    def setColumns(columns: List[ColumnDefinition]) = {
+    def setColumns(columns: List[ColumnDefinition]) {
       setVisibleColumns(columns.map(_.field).toArray)
       setColumnHeaders(columns.map(_.title).toArray)
     }
   }
 
-  class Tree(action: ItemClickEvent => Unit = null) extends com.vaadin.ui.Tree {
+  object Theme {
+    import com.vaadin.ui.themes._
+    def reindeer = new Reindeer
+    def runo = new Runo
+    def base = new BaseTheme
+    def liferay = new LiferayTheme
+  }
+
+  trait TreeExpand {
+    self: Tree =>
+    def onExpand(action: TreeExpandEvent => Unit) = {
+      addListener(new TreeExpandListener {
+        def nodeExpand(event: TreeExpandEvent) = action(event)
+      })
+    }
+  }
+
+  trait TreeCollapse {
+    self: Tree =>
+    def onCollapse(action: TreeCollapseEvent => Unit) = {
+      addListener(new TreeCollapseListener {
+        def nodeCollapse(event: TreeCollapseEvent) = action(event)
+      })
+    }
+  }
+
+  class Tree(caption: String = "", action: ItemClickEvent => Unit = null) extends com.vaadin.ui.Tree(caption) {
     if(action != null) {
       addListener(new ItemClickListener {
         def itemClick(event: ItemClickEvent) { action(event) }
@@ -161,7 +272,7 @@ package object scalaVaadin {
     new RichComponentContainer(container)
 
   class RichComponentContainer(container: com.vaadin.ui.ComponentContainer) {
-    def addComponents(components: com.vaadin.ui.Component*) { components.foreach(container.addComponent) }
+    def addComponents(components: com.vaadin.ui.Component*) { components.foreach(container.addComponent(_)) }
   }
 
   implicit def textFieldToRichTextField(textField: TextField) = new RichTextField(textField)
@@ -176,7 +287,7 @@ package object scalaVaadin {
 
   class Button(text: String, action: ButtonClickEvent => Unit = null) extends com.vaadin.ui.Button(text, new ButtonClickListener(action))
 
-  class WindowCloseListener(action: WindowCloseEvent => Unit) extends Window.CloseListener {
+  class WindowCloseListener(action: WindowCloseEvent => Unit) extends com.vaadin.ui.Window.CloseListener {
     def windowClose(event: WindowCloseEvent) { action(event) }
   }
 
